0a84b12123102d1fbf8450385af6655e680e7ad1
WojciechBiolik <wbiolik@gmail.com>
2022-11-12 13:27:56 +0100

    First test

---
+++ src/main/java/pl/coderslab/MainDao.java
@@ -0,0 +1,17 @@
+package pl.coderslab;
+
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+public class MainDao {
+    public static void main(String[] args) {
+        User user = new User();
+
+        user.setUserName("Wojtek");
+        user.setEmail("Wojtek@yahoo.com");
+        user.setPassword("Szczerbate krasnale");
+
+        UserDao userDao = new UserDao();
+        userDao.create(user);
+    }
+}
